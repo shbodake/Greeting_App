8559a5f9f1d043d02c0e128305b885bec0ce1cf0
shbodake <sayalibodake28@gmail.com>
2021-12-19 01:22:21 +0530

    Added: Returning JSON for different Http methds

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Greeting;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /*
+     *curl localhost:8080/greeting
+     @return={id =1 , content="hello world!"}
+     * curl localhost:8080/greeting?name=sayali
+     * @return= { id=2, content="hello sayali!" }
+     */
+    
+    @GetMapping(value={"/greeting","/greeting/","/greeting/home"})
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+    /*
+    *curl localhost:8080/greeting/sayali
+    @return={id =1 , content="hello sayali!}
+     */
+    
+    @GetMapping("greeting/{name}")
+    public Greeting greetings(@PathVariable String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}

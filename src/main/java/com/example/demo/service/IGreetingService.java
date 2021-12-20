2cdc51eb3411919230e201008432f332565acadf
shbodake <sayalibodake28@gmail.com>
2021-12-20 18:20:20 +0530

    Added: Greeting message with firstname and lastname

--- src/main/java/com/example/demo/service/IGreetingService.java
+++ src/main/java/com/example/demo/service/IGreetingService.java
@@ -1,7 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UserDto;
 import com.example.demo.model.Greeting;
 
 public interface IGreetingService {
 	Greeting greetingMessage();
+	String greetingMessageByName(UserDto userDto);
 }
